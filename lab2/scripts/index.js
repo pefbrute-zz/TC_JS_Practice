793291596705db081ac8edf73eedf2edbe781499
pefbrute <proggraming@mail.ru>
2020-04-10 18:21:04 +0300

    +labs

--- lab2/scripts/index.js
+++ lab2/scripts/index.js
@@ -1,9 +1,22 @@
-var a,b,x,F;
-a = prompt('Введите аргумент a', 0);
+var key;
 do {
-    b = prompt('Введите аргумент b, который > 0', 0);
-    if(b <= 0){alert('Введённый аргумент меньше или равен 0')};    
-} while (b <= 0);
-x = prompt('Введите аргумент x', 0);
-F = ((2*(8 * Math.pow(a,2) - 14 * a * b * x + 2 * Math.pow(b,2) * Math.pow(x,2))) / 15 * Math.pow(b,3)) * Math.sqrt(a + b * x);
-alert('Значение функции F = ' + F);
+  key = prompt("Перевести киллометры в мили?(0)\n"
+            + "Перевести мили в киллометры?(1)",0);
+  if (key < '0' || key > '1'){alert('Неправильно введено число')};
+}while(key < '0' || key > '1');
+switch (key) {
+    case '0':
+      do{
+        key = prompt('Сколько киллометров перевести в мили?', 0);
+        if (isNaN(key) == true){alert('Вы ввели строку, введите число')};
+      }while(isNaN(key) == true);
+      alert('В ' + key + ' километров ' + key * 0.621371 + ' миль');
+      break;
+    case '1':
+      do{
+        key = prompt('Сколько миль перевести в киллометры?', 0);
+        if (isNaN(key) == true){alert('Вы ввели строку, введите число')};
+      }while(isNaN(key) == true);
+      alert('В ' + key + ' миль ' + key * 1.60934 + ' киллометров')
+      break;
+  }
