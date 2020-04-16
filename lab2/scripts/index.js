15cc57cd2406c66f9c90efdddcdb198d0d45c661
pefbrute <proggraming@mail.ru>
2020-04-16 22:36:59 +0300

    +nothing

--- lab2/scripts/index.js
+++ lab2/scripts/index.js
@@ -1,25 +1,25 @@
-function convert(){
+function convert() {
   var key;
   do {
     key = prompt("Перевести киллометры в мили?(0)\n"
-              + "Перевести мили в киллометры?(1)", 0);
+                + "Перевести мили в киллометры?(1)", 0);
     key = parseInt(key);
-    if (key != 0 && key != 1){alert('Неправильно введено число')};
-    if (isNaN(key) == true){alert('Вы ввели строку, введите число')};
-  }while(key != 0 && key != 1 || isNaN(key) == true);
+    if (key != 0 && key != 1) { alert('Неправильно введено число') };
+    if (isNaN(key) == true) { alert('Вы ввели строку, введите число') };
+  } while (key != 0 && key != 1 || isNaN(key) == true);
   switch (key) {
     case 0:
-      do{
+      do {
         key = prompt('Сколько киллометров перевести в мили?', 3);
-        if (isNaN(key) == true){alert('Вы ввели строку, введите число')};
-      }while(isNaN(key) == true);
+        if (isNaN(key) == true) { alert('Вы ввели строку, введите число') };
+      } while (isNaN(key) == true);
       alert('В ' + key + ' километров ' + key * 0.62137 + ' миль');
       break;
     case 1:
-      do{
+      do {
         key = prompt('Сколько миль перевести в киллометры?', 4);
-        if (isNaN(key) == true){alert('Вы ввели строку, введите число')};
-      }while(isNaN(key) == true);
+        if (isNaN(key) == true) { alert('Вы ввели строку, введите число') };
+      } while (isNaN(key) == true);
       alert('В ' + key + ' миль ' + key * 1.60934 + ' киллометров')
       break;
   }
--- lab3/scripts/index.js
+++ lab3/scripts/index.js
@@ -1,4 +1,4 @@
-function fTable(){
+function fTable() {
   var x1,
     x2,
     dx,
@@ -8,36 +8,37 @@ function fTable(){
   // add table cleaning
   do {
     x1 = prompt('Введите целое число x1', 2);
-    if (isNaN(x1) == true){alert('Вы ввели строку, введите число')};
-    if (parseInt(x1) % 1 != 0){alert('Вы ввели вещественное число, введите целое')};
-  }while(isNaN(x1) == true || x1 % 1 != 0);
+    if (isNaN(x1) == true) { alert('Вы ввели строку, введите число') };
+    if (parseInt(x1) % 1 != 0) { alert('Вы ввели вещественное число, введите целое') };
+  } while (isNaN(x1) == true || x1 % 1 != 0);
   do {
     x2 = prompt('Введите число x2 не равное ' + x1, 3);
-    if (x1 == x2){alert('Вы ввели x2 равное ' + x1)};
-    if (isNaN(x2) == true){alert('Вы ввели строку, введите число')};
-    if (parseInt(x1) % 1 != 0){alert('Вы ввели вещественное число, введите целое')};
-  }while(isNaN(x2) == true);
+    if (x1 == x2) { alert('Вы ввели x2 равное ' + x1) };
+    if (isNaN(x2) == true) { alert('Вы ввели строку, введите число') };
+    if (parseInt(x1) % 1 != 0) { alert('Вы ввели вещественное число, введите целое') };
+  } while (isNaN(x2) == true);
   do {
-    if (x1 < x2){
-      do{
+    if (x1 < x2) {
+      do {
         dx = prompt('Введите число dx > 0', 0.5);
-        if (dx == 0){alert('Вы ввели dx = 0, dx не должен равняться 0')};
-      }while(dx <= 0);
-    }else{
-      do{
+        if (dx == 0) { alert('Вы ввели dx = 0, dx не должен равняться 0') };
+      } while (dx <= 0);
+    } else {
+      do {
         dx = prompt('Введите число dx < 0', -0.5);
-        if (dx == 0){alert('Вы ввели dx = 0, dx не должен равняться 0')};
-      }while(dx >= 0);
+        if (dx == 0) { alert('Вы ввели dx = 0, dx не должен равняться 0') };
+      } while (dx >= 0);
     };
-    if (isNaN(dx) == true){alert('Вы ввели строку, введите число')};
-  }while(isNaN(dx) == true);
+    if (isNaN(dx) == true) { alert('Вы ввели строку, введите число') };
+  } while (isNaN(dx) == true);
   table = document.getElementById('table');
   row = document.createElement('tr');
   xD = document.createElement('td');
   yD = document.createElement('td');
-  if (x1 < x2){
-    while (x1 <= x2){
-      if (Math.cos(x1) == 0){
+  // solve problem with float dx
+  if (x1 < x2) {
+    while (x1 <= x2) {
+      if (Math.cos(x1) == 0) {
         continue;
       };
       y = (x1 - 5) / Math.cos(x1);
@@ -51,10 +52,10 @@ function fTable(){
       table.appendChild(row);
       x1 = parseInt(x1) + parseInt(dx);
     }
-  }else{
-    while(x1 >= x2){
-      if (Math.cos(x1) == 0){
-       continue;
+  } else {
+    while (x1 >= x2) {
+      if (Math.cos(x1) == 0) {
+        continue;
       };
       y = (x1 - 5) / Math.cos(x1);
       row = document.createElement('tr');
