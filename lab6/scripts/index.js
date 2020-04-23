6309b0279a86238c682c4b404fe0108da183ce28
pefbrute <proggraming@mail.ru>
2020-04-23 12:11:47 +0300

    +whitespaces-cleaning +split-words-from-string-to-an-array

--- lab6/scripts/index.js
+++ lab6/scripts/index.js
@@ -1,8 +1,29 @@
 function minWords(){
-  var string = null;
+  var 
+  //try don't assign string 
+      string = null,
+      words = [],
+      minWords = [];
   do {
-    string = prompt("Введите строку", 2);
-    console.log(string);
+    string = prompt("Введите какую-нибудь строку", 2);
+    string = string.replace(/\s+/g,' ');
     if (string == '' || string == ' '){alert('Вы ничего не ввели')};
-  }while(string == null);
+  }while(string == '' || string == ' ');
+  words = string.split(' ');
+  if (string.indexOf(' ') >= 0){
+    words = string.split(' ');
+  }else{
+    alert('Минимальное слово: ', string);
+    break;
+  };
+  minWords[0] = words[0];
+  j = 0;
+  for(var i = 1; i < words.length; i++){
+    if (minWords[0].length > words[i].length){
+      minWords[0] = words[i];
+    }else if(minWords[0].length == words[i].length){
+      j++;
+      minWords[j] = words[i];
+    };
+  }
 }
